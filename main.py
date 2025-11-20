a512e959abb4b32f1fe7416b78e5264344c57b90
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-11-20 06:08:07 +0200

    ul3

--- main.py
+++ main.py
@@ -27,23 +27,36 @@ sky_surface = pygame.image.load("".join([os.path.dirname(__file__), "/graphics/s
 ground_surface = pygame.image.load("".join([os.path.dirname(__file__), "/graphics/ground.png"])).convert_alpha()
 text_surface = test_font.render("Hello, World!", False, "White")
 snail_surface = pygame.image.load("".join([os.path.dirname(__file__), "/graphics/snail/snail1.png"])).convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (900, 300))
+player_surface = pygame.image.load("".join([os.path.dirname(__file__), "/graphics/player/player_walk_1.png"])).convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
     # Check all possible types of inputs
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision')
     
     # Attach surfaces
     screen.blit(sky_surface, sur_pos_origin)
     screen.blit(ground_surface, sur_pos_ground)
     screen.blit(text_surface, sur_pos_text)
-    snail_x_pos -= 1
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 270))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print("Collision!")
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
 
     # Draw all elements, update everything
     pygame.display.update()
