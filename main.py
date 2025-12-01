dab8c5f9ea5b75344082c8b16ca5b98f45f1cf27
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2025-12-01 05:03:42 +0200

    ul18

--- main.py
+++ main.py
@@ -9,36 +9,51 @@ class Screen():
         self.scr_wid = 800
         self.scr_hgt = 400
         self.scr = pygame.display.set_mode((self.scr_wid, self.scr_hgt))
-        self.caption = pygame.display.set_caption("TODO")
         self.clock = pygame.time.Clock()
         self.framerate = 60  # 1/[s]
 
+# Background
+class Background():
+
+    def __init__(self):
+        try:
+            self.ground_surf = pygame.image.load(
+            os.path.join(os.path.dirname(__file__), "graphics", "ground.png")
+            ).convert_alpha()
+            self.sky_surf = pygame.image.load(
+            os.path.join(os.path.dirname(__file__), "graphics", "sky.png")
+            ).convert_alpha()
+        except FileNotFoundError:
+            print("Background files not found")
+        self.ground_pos = (0, 300)
+        self.sky_pos = (0, 0)
+
+    def draw(self, screen):
+        screen.blit(self.sky_surf, self.sky_pos)
+        screen.blit(self.ground_surf, self.ground_pos)
+
+# Player
+class Player():
+
+    def __init__(self):
+        try:
+            self.player_surf = pygame.image.load(
+                os.path.join(os.path.dirname(__file__), "graphics", "player",
+                "player_stand.png")).convert_alpha()
+            self.player_rect = self.player_surf.get_rect(
+                midbottom = (100, 300))
+        except FileNotFoundError:
+            print("Player file not found")
+
+    def draw(self, screen):
+        screen.blit(self.player_surf, self.player_rect)
+
+# Initialize Pygame
+pygame.init()
+pygame.display.set_caption("TODO")
 screen = Screen()
-
-# Background Surface
-ground_surf = pygame.image.load(
-            "".join(
-                [os.path.dirname(__file__), "/graphics/ground.png"]
-                )).convert_alpha()
-
-sky_surf = pygame.image.load(
-            "".join(
-                [os.path.dirname(__file__), "/graphics/sky.png"]
-                )).convert_alpha()
-
-# Background Position
-ground_pos = (0, 300)
-sky_pos = (0, 0)
-
-# Player Surface
-player_surf = pygame.image.load(
-            "".join(
-                [os.path.dirname(__file__), "/graphics/player/"
-                "player_stand.png"]
-                )).convert_alpha()
-
-# Player Rectangle
-player_rect = player_surf.get_rect(midbottom = (100, 300))
+background = Background()
+player = Player()
 
 # Game Loop
 while True:
@@ -47,8 +62,7 @@ while True:
             pygame.quit()
             exit()
 
-    screen.scr.blit(sky_surf, sky_pos)
-    screen.scr.blit(ground_surf, ground_pos)
-    screen.scr.blit(player_surf, player_rect)
+    background.draw(screen.scr)
+    player.draw(screen.scr)
     pygame.display.update()
     screen.clock.tick(screen.framerate)
